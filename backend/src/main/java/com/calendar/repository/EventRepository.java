07df001f3f5813c0e421c67fd90af72a533e278a
bhuvana1896 <bhuvanakruthi1896@gmail.com>
2026-04-02 21:41:19 +0530

    Delete backend directory

--- backend/src/main/java/com/calendar/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.calendar.repository;
-
-import com.calendar.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
